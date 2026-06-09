dafe1078dc6802f79220aeb842d208daaf6a0bdf
Spein <alispein93@gmail.com>
2026-06-09 12:44:41 +0300

    Update cache name and add new URLs to cache

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
-const CACHE_NAME = "smatibabu-v1";
+const CACHE_NAME = "matibabu-cache-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  "./",
+  "./user.html",
+  "./admin.html"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
